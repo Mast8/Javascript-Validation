0e4cc7ed5808301362a7111b6bd1b94912f0fa4e
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-04-27 16:28:27 -0400

    new way

--- main.js
+++ main.js
@@ -5,33 +5,35 @@ const errorElement = document.getElementById('error');
 const errorElementpassword = document.getElementById('errorP');
 
 form.addEventListener('submit', (e) => {
-  let messages = {};
+  //let messages = {name: "", password: "" };
+  let messages = { };
   if (names.value === '' || names.value == null || names.value .trim() === "") {
     messages.name = 'Name is required';
   } 
 
-  if (password.value.length <= 6) {
+  if (password.value.length <= 6) 
     messages.password = 'Password must be longer than 6 characters';
+  else {
+    if (password.value.length >= 20) {
+      messages.password = 'Password must be less than 20 characters';
+    } else {
+      if (password.value === 'password') 
+        messages.password = 'Password cannot be password';
+      //else messages.password = '';
+    }
   }
 
-  if (password.value.length >= 20) {
-    messages.password = 'Password must be less than 20 characters';
-  }
+  
 
-  if (password.value === 'password') {
-    messages.password = 'Password cannot be password';
-  }
+  
 
   if (Object.keys(messages).length > 0) {
-    console.log(messages);
+    console.log(messages.length);
     e.preventDefault();
-    //errorElement.innerText = messages.join(', ');
-    if(messages.name )
-    errorElement.innerText = messages.name;
+    if( messages.name )
+      errorElement.innerText = messages.name;
+    //if( messages.password )
     errorElementpassword.innerText = messages.password;
-  }else {
-    messages.name = "";
-    //messages.name = ' ';
   }
 
 })
