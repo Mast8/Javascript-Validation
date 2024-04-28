d01dfa89bdc9ad25b9d5f53260424df796f54f28
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-04-28 14:16:19 -0400

    repeat password field

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const name = document.getElementById('names');
 const password = document.getElementById('password');
+const password1 = document.getElementById('password1');
+
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 const errorElementpassword = document.getElementById('errorP');
+const errorElementpassword1 = document.getElementById('errorP1');
 
 form.addEventListener('submit', (e) => {
 
@@ -18,11 +21,16 @@ form.addEventListener('submit', (e) => {
     if (password.value.length >= 20) {
       messages.password = 'Password must be less than 20 characters';
     } else {
-      if (password.value === 'password') 
+      if (password.value === 'password') {
         messages.password = 'Password cannot be password';
+      }
       else errorElementpassword.innerText = "";
     }
   }
+
+  if( password1.value !== password.value ) {
+    messages.password1 = 'Passwords are not equal';
+  }else errorElementpassword1.innerText = "";
 
   //if there are errors 
   if (Object.keys(messages).length > 0) {
@@ -32,7 +40,9 @@ form.addEventListener('submit', (e) => {
     if( messages.name )
       errorElement.innerText = messages.name;
     if( messages.password )
-    errorElementpassword.innerText = messages.password;
-  } else  messages={}; 
+      errorElementpassword.innerText = messages.password;
+    if(messages.password1 )
+      errorElementpassword1.innerText = messages.password1;
+  } 
 
 })
