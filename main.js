18dd505d9827f3c8a39030b3dbecefb07c4ea061
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-05-01 12:46:21 -0400

    amost done

--- main.js
+++ main.js
@@ -12,62 +12,69 @@ const errorElementEmail= document.getElementById('errorEm');
 form.addEventListener('submit', (e) => {
 
   //write the validation messages
-  let messages = { };
-  if (names.value === '' || names.value == null || names.value .trim() === "") {
-    messages.name = 'Name is required';
-  } else { 
-      showSuccess(names );
-      errorElement.innerText = "";
-  }
+  
 
-  if (password.value.length <= 6) 
-    messages.password = 'Password must be longer than 6 characters';
-  else {
-    if (password.value.length >= 20) {
-      messages.password = 'Password must be less than 20 characters';
-    } else {
-      if (password.value === 'password') {
-        messages.password = 'Password cannot be password';
-       
-      }
-      else errorElementpassword.innerText = "";
-      showSuccess(password );
-    }
-  }
 
-  if( password1.value !== password.value || password1.value.trim() === "" ) {
-    messages.password1 = 'Passwords are not equal';
-  } else {
-      errorElementpassword1.innerText = "";
-      showSuccess(password1);
-    }
+
 
   
-  let re = /\S+@\S+\.\S+/;
-
-  if( !re.test(email.value)  ){
-    messages.email = 'Has to be a valid email';
-  } else errorElementEmail.innerText = "";
-
-  //if there are errors 
-  if (Object.keys(messages).length > 0) { 
   
-    console.log(messages);
+    
     e.preventDefault();
     if(checkRequired([names, email, password, password1])){
+     
       //get and show each error
-      if( messages.name )
-        errorElement.innerText = messages.name;
-      if( messages.password )
-        errorElementpassword.innerText = messages.password;
-      if(messages.password1 )
-        errorElementpassword1.innerText = messages.password1;
-      if( messages.email)
-        errorElementEmail.innerText = messages.email;
+      
+     
+      checkEmail(email) ;
+      checkLength(password, 6, 20);
+      checkPasswordsMatch(password, password1);
+     /*  if(checkEmail(email)  && checkLength(password, 6, 20) &&  checkPasswordsMatch(password, password1)) {
+        console.log("Success");
+      }  */
     }
-  } 
+  
 
 })
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //let passed = false;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+    //passed = true;
+  } else {
+    showError(input, 'Email is not valid');
+  }
+  //return passed;
+}
+
+
+function checkLength(input, min, max) {
+  //let passed = false;
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+    //passed = true;
+  }
+  //return passed;
+}
+
+function checkPasswordsMatch(input1, input2) {
+  //let passed = false;
+    if (input1.value !== input2.value) {
+      showError(input2, 'Passwords do not match');
+    }//else passed = true;
+    //return passed;
+}
+
 
 function checkRequired(inputArr) {
   let isRequired = false;
@@ -92,8 +99,9 @@ function showError(input, message) {
   console.log(message);
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
+
   const small = formControl.querySelector('small');
-  console.log(small);
+  //console.log(small);
   small.innerText = message;
 }
 
